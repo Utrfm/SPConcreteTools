be6e4b8da77d3881f51494f484072f1e17ef488a
Utrfm <utrfm25@gmail.com>
2022-01-09 15:05:36 +0300

    Update

--- src/modules/sofistik_cdb/main.py
+++ src/modules/sofistik_cdb/main.py
@@ -12,8 +12,8 @@ from sofistik_daten import *
 # This example has been tested with Python 3.7 (64-bit)
 # print ("The path variable=", os.environ["Path"])
 
-print ("Hint: 64bit DLLs are used")
-path = os.environ["Path"]
+# print ("Hint: 64bit DLLs are used")
+# path = os.environ["Path"]
 
 # 64bit DLLs
 os.add_dll_directory(r"C:\Program Files\SOFiSTiK\2020\SOFiSTiK 2020\interfaces\64bit")
@@ -60,68 +60,92 @@ do while ie == 0, see cdbase.chm, Returnvalue.
     = 3 -> Key does not exist
 """
 
-#read and save list of loadCases
-lc_all = list(range(1,9999))
-lc = []
-for i in lc_all:
-    if py_sof_cdb_kexist(12,i):
-        lc.append(i)
-print(f'{lc=}')
-
-class Node():
-    def __init__(self,nr,kfix=127,xyz=(0,0,0),pz=0):
-        self.nr = nr
-        self.kfix = kfix
-        self.xyz = xyz
-    Pz = 0
-
-#read and save nodes with supports
-k1 = 20
+#read and save list of structural areas
+areas = []
+k1 = 32
 k2 = 0
-func = cnode
-nodes = []
+func = cgln_matc
 
 if py_sof_cdb_kexist(k1, k2) == 2: # the key exists and contains data
     ie = c_int(0)
     RecLen = c_int(sizeof(func))
     while ie.value == 0:
         ie.value = py_sof_cdb_get(Index, k1, k2, byref(func), byref(RecLen), 1)
-        if func.m_nr > 0:
-            if func.m_kfix !=127:
-                xyz = (func.m_xyz[0],func.m_xyz[1],func.m_xyz[2])
-                nodes.append(Node(func.m_nr,func.m_kfix,xyz))
-        RecLen = c_int(sizeof(func))
+        areas.append(func.m_nr)
 
-def Clean_Nodes(nodes):
-    nr = []
-    clean_nodes = []
-    for n in nodes:
-        if n.nr not in nr:        
-            nr.append(n.nr)
-            clean_nodes.append(n)
-    return(nr,clean_nodes)
+print(f'{areas=}')
 
-(node_numbers,nodes) = Clean_Nodes(nodes)
-# print(f'I have nodes {node_numbers}')
-# for n in nodes:
-#     print(n.nr,n.kfix,n.xyz)
-
-
-#read support forces
-k1 = 24
-k2 = 1
-func = cn_disp
+k1 = 32
+k2 = 1003
+func = cgar
 
 if py_sof_cdb_kexist(k1, k2) == 2: # the key exists and contains data
     ie = c_int(0)
     RecLen = c_int(sizeof(func))
     while ie.value == 0:
         ie.value = py_sof_cdb_get(Index, k1, k2, byref(func), byref(RecLen), 1)
-        if func.m_nr in node_numbers:
-            for n in nodes:
-                if func.m_nr == n.nr:
-                    n.Pz = func.m_pz
-        RecLen = c_int(sizeof(func))
+        # if func.m_id==T:
+        print(f'group {func.m_nog=}')
+        print(f'thickness {func.m_td[0]=} m')
+        # if func.m_id==10:
+        #     print(f'{func.m_box[1][1]=}')
+
+
+
+# class Node():
+#     def __init__(self,nr,kfix=127,xyz=(0,0,0),pz=0):
+#         self.nr = nr
+#         self.kfix = kfix
+#         self.xyz = xyz
+#     Pz = 0
+
+# #read and save nodes with supports
+# k1 = 20
+# k2 = 0
+# func = cnode
+# nodes = []
+
+# if py_sof_cdb_kexist(k1, k2) == 2: # the key exists and contains data
+#     ie = c_int(0)
+#     RecLen = c_int(sizeof(func))
+#     while ie.value == 0:
+#         ie.value = py_sof_cdb_get(Index, k1, k2, byref(func), byref(RecLen), 1)
+#         if func.m_nr > 0:
+#             if func.m_kfix !=127:
+#                 xyz = (func.m_xyz[0],func.m_xyz[1],func.m_xyz[2])
+#                 nodes.append(Node(func.m_nr,func.m_kfix,xyz))
+#         RecLen = c_int(sizeof(func))
+
+# def Clean_Nodes(nodes):
+#     nr = []
+#     clean_nodes = []
+#     for n in nodes:
+#         if n.nr not in nr:        
+#             nr.append(n.nr)
+#             clean_nodes.append(n)
+#     return(nr,clean_nodes)
+
+# (node_numbers,nodes) = Clean_Nodes(nodes)
+# # print(f'I have nodes {node_numbers}')
+# # for n in nodes:
+# #     print(n.nr,n.kfix,n.xyz)
+
+
+# #read support forces
+# k1 = 24
+# k2 = 1
+# func = cn_disp
+
+# if py_sof_cdb_kexist(k1, k2) == 2: # the key exists and contains data
+#     ie = c_int(0)
+#     RecLen = c_int(sizeof(func))
+#     while ie.value == 0:
+#         ie.value = py_sof_cdb_get(Index, k1, k2, byref(func), byref(RecLen), 1)
+#         if func.m_nr in node_numbers:
+#             for n in nodes:
+#                 if func.m_nr == n.nr:
+#                     n.Pz = func.m_pz
+#         RecLen = c_int(sizeof(func))
 
 # print('Add support forces')
 # for n in nodes:
