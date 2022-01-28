54dd098d85375007b5713d1611d188ac1d164e0a
Utrfm <utrfm25@gmail.com>
2022-01-28 06:42:35 +0300

    'update program structure'

---
+++ src/modules/calc_model.py
@@ -0,0 +1,5 @@
+from elements.elements import *
+# from sofistik_cdb import *
+
+a = AreaElement(100)
+print(a.id_fe)
--- src/modules/cdbwork/cdbwork — копия.py
+++ src/modules/cdbwork/cdbwork — копия.py
@@ -7,7 +7,7 @@ import os             # for the environment variable necessary
 import platform       # checks the python platform
 from ctypes import *  # read the functions from the cdb
 from sofistik_daten import *
-from elements import *
+from ..classes import *
 
 from pathlib import PureWindowsPath
 
@@ -71,113 +71,64 @@ if py_sof_cdb_kexist(key1, key2) == 2: # the key exists and contains data
     RecLen = c_int(sizeof(func))
     while ie.value == 0:
         ie.value = py_sof_cdb_get(Index, key1, key2, byref(func), byref(RecLen), 1)
-        structural_areas.append(func.m_nr)
+        structural_areas.append(Area(func.m_nr))
 
-print(f'{structural_areas=}')
+#read cdb and update existing Area class parameters
 
-# for a in structural_areas:
-
-#     key1 = 32
-#     key2 = a
-#     func = cgar
-
-#     if py_sof_cdb_kexist(key1, key2) == 2: # the key exists and contains data
-#         ie = c_int(0)
-#         RecLen = c_int(sizeof(func))
-#         while ie.value == 0:
-#             ie.value = py_sof_cdb_get(Index, key1, key2, byref(func), byref(RecLen), 1)
-#             # print(f'{func.m_id=}')
-#             # if func.m_id==0:
-#             #     print(f'group {func.m_nog=}')
-#             #     print(f'thickness {func.m_td[0]=} m')
-#         # if func.m_id==10:
-#         #     print(f'{func.m_box[1][1]=}')
-def SarsRead(int_id):
+def SarsRead(area):
     key1 = 39
     key2 = 0
     func = csar
 
     print(f'{py_sof_cdb_kexist(key1, key2)=}')
     if py_sof_cdb_kexist(key1, key2) == 2: # the key exists and contains data
-        
         ie = c_int(0)
         RecLen = c_int(sizeof(func))
         while ie.value < 2:
-        # while ie.value == 0:
             ie.value = py_sof_cdb_get(Index, key1, key2, byref(func), byref(RecLen), 1)
-            # print(f'{ie.value=}')
-            if func.m_id == 200 and func.m_ids==1001:
+            if func.m_id == 200 and func.m_ids==area.id:
+                area.group = func.m_nog
                 print(f'group {func.m_nog=}')
+                area.material = func.m_nom
                 print(f'material {func.m_nom=}')
-                print(f'group {func.m_nog=}')
+                area.thickness = round(func.m_td[0],3)
                 print(f'thickness {func.m_td[0]=}')
-                print(f'thickness {func.m_td[2]=}')
+
+    #read cdb and update area quad elements list
+    key1 = 32
+    key2 = area.id
+    func = cgar_elnr
+
+    if py_sof_cdb_kexist(key1, key2) == 2: # the key exists and contains data  
+        fe = []
+        ie = c_int(0)
+        RecLen = c_int(sizeof(func))
+        while ie.value < 2:
+            ie.value = py_sof_cdb_get(Index, key1, key2, byref(func), byref(RecLen), 1)
+            if func.m_id == 121:
+                fe.append(func.m_nr[0])
+                fe.append(func.m_nr[1])
+                area.elements = fe
+                print(f'elements {fe}')
+                break
+    
 
 
+a = structural_areas[4]
+SarsRead(a)
 
-SarsRead(1004)
-# class Node():
-#     def __init__(self,nr,kfix=127,xyz=(0,0,0),pz=0):
-#         self.nr = nr
-#         self.kfix = kfix
-#         self.xyz = xyz
-#     Pz = 0
+print(f'{a.id=}')
+print(f'{a.group=}')
+print(f'{a.thickness=}')
+print(f'{a.elements=}')
 
-# #read and save nodes with supports
-# k1 = 20
-# k2 = 0
-# func = cnode
-# nodes = []
-
-# if py_sof_cdb_kexist(k1, k2) == 2: # the key exists and contains data
-#     ie = c_int(0)
-#     RecLen = c_int(sizeof(func))
-#     while ie.value == 0:
-#         ie.value = py_sof_cdb_get(Index, k1, k2, byref(func), byref(RecLen), 1)
-#         if func.m_nr > 0:
-#             if func.m_kfix !=127:
-#                 xyz = (func.m_xyz[0],func.m_xyz[1],func.m_xyz[2])
-#                 nodes.append(Node(func.m_nr,func.m_kfix,xyz))
-#         RecLen = c_int(sizeof(func))
-
-# def Clean_Nodes(nodes):
-#     nr = []
-#     clean_nodes = []
-#     for n in nodes:
-#         if n.nr not in nr:        
-#             nr.append(n.nr)
-#             clean_nodes.append(n)
-#     return(nr,clean_nodes)
-
-# (node_numbers,nodes) = Clean_Nodes(nodes)
-# # print(f'I have nodes {node_numbers}')
-# # for n in nodes:
-# #     print(n.nr,n.kfix,n.xyz)
-
-
-# #read support forces
-# k1 = 24
-# k2 = 1
-# func = cn_disp
-
-# if py_sof_cdb_kexist(k1, k2) == 2: # the key exists and contains data
-#     ie = c_int(0)
-#     RecLen = c_int(sizeof(func))
-#     while ie.value == 0:
-#         ie.value = py_sof_cdb_get(Index, k1, k2, byref(func), byref(RecLen), 1)
-#         if func.m_nr in node_numbers:
-#             for n in nodes:
-#                 if func.m_nr == n.nr:
-#                     n.Pz = func.m_pz
-#         RecLen = c_int(sizeof(func))
-
-# print('Add support forces')
-# for n in nodes:
-#     print(n.nr,n.kfix,n.xyz,n.Pz)
+#read cdb and create list of loadcases
 
 
 
 
+#read cdb and create quad finite elements for selected groups/structural areas
+#elements contains forces from selected load cases
 
 
 
